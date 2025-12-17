a1c099f8c300dc370f7b1dcabb442ad95ce0cab5
yKaike777 <kaike.barros@aluno.senai.br>
2025-12-17 20:35:31 -0300

    Fix: Usuários agora podem sacar e adicionar saldo

--- auth/signup.php
+++ auth/signup.php
@@ -4,11 +4,12 @@
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $balance = 0;
 
-    $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO users (username, email, password, balance) VALUES (?, ?, ?, ?)";
 
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $username, $email, $password);
+    $stmt->bind_param("sssd", $username, $email, $password, $balance);
 
     if ($stmt->execute()) {
         header("Location: ../login.html");
